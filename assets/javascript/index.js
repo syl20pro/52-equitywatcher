0b23b02f9ef831ccb0d8b06b281e9d91bf5374aa
Sylvain Gemieux <44305327+syl20pro@users.noreply.github.com>
2021-06-26 22:25:55 +0200

    test api
    test de l'api + création du fichier index.js

---
+++ assets/javascript/index.js
@@ -0,0 +1,7 @@
+const form = document.getElementById('searchForm')
+const searchInput = document.getElementById('searchInput')
+const result = document.getElementById('result')
+
+const fetchTickers = async () {
+    
+}
