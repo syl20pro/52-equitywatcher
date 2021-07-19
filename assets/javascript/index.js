02a2f88ce2499d6da9ca1f182802ac1534b411b1
Sylvain Gemieux <44305327+syl20pro@users.noreply.github.com>
2021-07-19 22:27:09 +0200

    change[js]:delete old folder
    suppression du répertoire javascript remplacer par le répertoire js

--- assets/javascript/index.js
+++
@@ -1,7 +0,0 @@
-const form = document.getElementById('searchForm')
-const searchInput = document.getElementById('searchInput')
-const result = document.getElementById('result')
-
-const fetchTickers = async () {
-    
-}
